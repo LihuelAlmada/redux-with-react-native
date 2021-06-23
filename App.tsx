c4312e2777a1b4fe3a341cb073c1410cd3e859f5
Necralma <lihuel.almada@gmail.com>
2021-06-23 11:59:18 -0300

    add redux

--- App.tsx
+++ App.tsx
@@ -1,37 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import * as React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 import { Provider } from "react-redux";
-import { combineReducers, createStore } from 'redux';
-import * as reducers from "./src/reducers";
+import App from "./src";
+import store from "./src/store";
 
-
-const MiApp = () => {
-  return(
-    <View style={styles.container}>
-      <Text>Open up Aqdwqwdqwdwqd app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
-
-const store = createStore(combineReducers({
-  ...reducers,
-}))
-
-export default function App() {
+export default function () {
   return (
     <Provider store={store}>
-      <MiApp />
+      <App />
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
