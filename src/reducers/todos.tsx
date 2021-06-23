dde1de7842db3a54097a967251194f6be3ffd42f
Necralma <lihuel.almada@gmail.com>
2021-06-23 14:12:37 -0300

    Before to show list store

--- src/reducers/todos.tsx
+++ src/reducers/todos.tsx
@@ -1,4 +1,9 @@
-export default (state = [], action: any) =>{
+const initialState = [
+    {id: 1, desc: 'todo 1', completed: false},
+    {id: 2, desc: 'todo 2', completed: false},
+    {id: 3, desc: 'todo 3', completed: false},
+]
+export default (state = initialState, action: any) =>{
     console.log(action)
     return state
 }
