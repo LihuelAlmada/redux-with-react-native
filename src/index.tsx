82e094f29e42938ca36f384469f0dcb6812fc26c
Necralma <lihuel.almada@gmail.com>
2021-06-25 15:46:51 -0300

    add random animated

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Animated, StyleSheet, Text, View, FlatList, SafeAreaView, Button} from "react-native";
 import ListItem from "./components/ListItem";
 import { connect } from "react-redux";
 import Input from "./components/Input";
@@ -18,10 +18,44 @@ const styles = StyleSheet.create({
   list: {
     alignSelf: "stretch",
   },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue"
+  },
+  fadingText: {
+    fontSize: 28
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 16
+  }
 });
 //complete and submit no son los importados de reducers/todos, sino los atributos de mapDispatchToProps
 const App = ({ data, complete, submit }: any) => {
   const [value, setValue] = useState("");
+
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const numb:number = 1
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    } ).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
 
   const handleChange = (val: any) => {
     setValue(val);
@@ -35,6 +69,23 @@ const App = ({ data, complete, submit }: any) => {
 
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim
+          }
+        ]}
+      >
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
+        <View style={styles.buttonRow}>
+          <Button title="Fade In View" onPress={fadeIn} />
+          <Button title="Fade Out View" onPress={fadeOut} />
+        </View>
+      </SafeAreaView>
       <Text>Lista Productos</Text>
       <Input 
         onSubmit={handleSubmit} 
